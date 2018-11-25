28e880943b49de161d735bfa4ca3a857bf8fed9f
Cas van der Hoven <casvanderhoven@gmail.com>
2018-11-25 19:12:16 +0100

    Initial blog setup and design

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_11_25_145618) do
-=======
-=======
-ActiveRecord::Schema.define(version: 2018_11_25_140751) do
->>>>>>> parent of 2388477... Install lines-engine
+ActiveRecord::Schema.define(version: 2018_11_25_153025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +30,92 @@ ActiveRecord::Schema.define(version: 2018_11_25_140751) do
     t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 40
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", unique: true
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+  end
+
+  create_table "lines_articles", force: :cascade do |t|
+    t.string "title"
+    t.string "sub_title"
+    t.text "content"
+    t.boolean "published", default: false
+    t.datetime "published_at"
+    t.string "hero_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "slug"
+    t.string "gplus_url"
+    t.boolean "featured", default: false
+    t.string "document"
+    t.string "short_hero_image"
+    t.text "teaser"
+    t.index ["slug"], name: "index_lines_articles_on_slug", unique: true
+  end
+
+  create_table "lines_authorables", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_lines_authorables_on_article_id"
+    t.index ["author_id"], name: "index_lines_authorables_on_author_id"
+  end
+
+  create_table "lines_authors", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
+    t.string "gplus_profile"
+  end
+
+  create_table "lines_pictures", force: :cascade do |t|
+    t.string "image"
+    t.string "name"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_lines_pictures_on_article_id"
+  end
+
+  create_table "lines_users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "tag_id"
+    t.string "taggable_type"
+    t.bigint "taggable_id"
+    t.string "tagger_type"
+    t.bigint "tagger_id"
+    t.string "context", limit: 128
+    t.datetime "created_at"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+    t.index ["taggable_type", "taggable_id"], name: "index_taggings_on_taggable_type_and_taggable_id"
+    t.index ["tagger_type", "tagger_id"], name: "index_taggings_on_tagger_type_and_tagger_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
+  end
+
 end
->>>>>>> parent of 2388477... Install lines-engine
